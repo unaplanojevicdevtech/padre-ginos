af7c40dc10e6eb51473bfcd8b7decea659b59102
Una Planojevic <una.planojevic@devtechgroup.com>
2025-02-11 11:41:40 +0100

    #customHook

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Order from "./Order";
+import PizzaOfTheDay from "./PizzaOfTheDay";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="logo">Padre Gino's Pizza</h1>
-      <Order />
-    </div>
+    <StrictMode>
+      <div>
+        <h1 className="logo">Padre Gino's Pizza</h1>
+        <Order />
+        <PizzaOfTheDay />
+      </div>
+    </StrictMode>
   );
 };
 
