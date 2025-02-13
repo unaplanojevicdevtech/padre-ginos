151243ad73ae460607489149c90f2cc046fdb8de
Una Planojevic <una.planojevic@devtechgroup.com>
2025-02-13 11:01:31 +0100

    #tanstack_router

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
-import { StrictMode, useState } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Order from "./Order";
-import PizzaOfTheDay from "./PizzaOfTheDay";
-import Header from "./Header";
-import { CartContext } from "./contexts";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
 
 const App = () => {
-  const cartHook = useState([]);
   return (
     <StrictMode>
-      <CartContext.Provider value={cartHook}>
-        <div>
-          <Header />
-          <Order />
-          <PizzaOfTheDay />
-        </div>
-      </CartContext.Provider>
+      <RouterProvider router={router} />
     </StrictMode>
   );
 };
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
@@ -14,5 +16,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [TanStackRouterVite(), react()]
 });
